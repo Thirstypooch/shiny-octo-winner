b225766a9dd33172347adc6b51f2a7fe682e5b97
Thirstypooch <thirstypooch@protonmail.com>
2020-10-21 23:00:41 -0500

    setting up this bad boy 👅

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { MikroORM } from '@mikro-orm/core'
-import { __prod__ } from './constants'
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: 'lireddit',
-    type: 'postgresql',
-    user: 'postgres',
-    password: 'postgres',
-    debug: !__prod__,
-  })
-}
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+  console.log("--------------sql 2-------------------");
+  await orm.em.nativeInsert(Post, { title: "my second post" });
+};
 
-console.log('Thirsty Pooch')
+main().catch((err) => {
+  console.error(err);
+});
